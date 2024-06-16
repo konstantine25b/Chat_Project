ce9bc67d80ce3acdeb242bb32d15e25025b14bc9
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-06-16 12:53:28 +0400

    build fully functional chat that uses websockets

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -73,6 +73,14 @@ ASGI_APPLICATION = "chat_project.asgi.application"
 WSGI_APPLICATION = 'chat_project.wsgi.application'
 
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
